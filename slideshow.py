651622ac53edb6b47d35ffe9c48ab645a4a32d6a
Seabox02 <lorenzo.marcassa@studenti.unimi.it>
2026-02-14 10:23:41 +0100

    Aggiunte immagini di casa

--- slideshow.py
+++ slideshow.py
@@ -6,8 +6,11 @@ folder_path = 'pomodori-1/valid/images'
 model = YOLO('runs/segment/modello_pomodori/weights/best.pt')
 
 
-images = [f for f in os.listdir(folder_path) if f.endswith('.jpg')]
+images = [f for f in os.listdir(folder_path) if f.endswith('.jpg') or f.endswith('.jpeg')]
 current_index = 0
+
+def ridimensiona_smart(img):
+   return img if img.shape[:2] == (432, 432) else cv2.resize(img, (432, 432))
 
 print("COMANDI:")
 print(" [ D ] -> Prossima immagine")
@@ -18,13 +21,20 @@ while True:
     img_name = images[current_index]
     img_path = os.path.join(folder_path, img_name)
     
-    results = model.predict(img_path, conf=0.4, verbose=False)
+    frame = cv2.imread(img_path)
+    
+    if frame is None:
+        print(f"Impossibile leggere: {img_name}")
+        current_index = (current_index + 1) % len(images)
+        continue
+
+    frame_resized = ridimensiona_smart(frame)
+
+    results = model.predict(frame_resized, conf=0.4, verbose=False)
+
     annotated_frame = results[0].plot()
 
-    cv2.putText(annotated_frame, f"Img: {current_index+1}/{len(images)} - {img_name}", 
-                (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 255, 255), 2)
-
-    cv2.imshow("Navigatore Pomodori", annotated_frame)
+    cv2.imshow("Slideshow Pomodori (Auto-Size)", annotated_frame)
 
     key = cv2.waitKey(0) 
 
